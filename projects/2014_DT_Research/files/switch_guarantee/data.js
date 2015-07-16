80612869715587cd9dc0b2abeeddb5620e67c903
lunamari9 <mpietrowiak@gmail.com>
2015-07-15 20:22:35 -0400

    adding project files.

---
+++ projects/2014_DT_Research/files/switch_guarantee/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,[i,j,k,l],m,_(n,o,p,q,r,s,t,_(),u,_(v,w,x,y,z,_(A,B,C,D),E,null,F,y,G,y,H,I,J,null,K,L,M,N,O,P,Q,L),R,_(),S,_(),T,_(U,[])),V,_(),W,_());}; 
+var b="url",c="switch_guarantee.html",d="generationDate",e=new Date(1418392127247.38),f="isCanvasEnabled",g=false,h="variables",i="OnLoadVariable",j="ProductReco",k="Digital",l="Balance",m="page",n="packageId",o="5aacc405f5364e1a8908647fef5a4258",p="type",q="Axure:Page",r="name",s="Switch Guarantee",t="notes",u="style",v="baseStyle",w="627587b6038d43cca051c114ac41ad32",x="pageAlignment",y="near",z="fill",A="fillType",B="solid",C="color",D=0xFFFFFFFF,E="image",F="imageHorizontalAlignment",G="imageVerticalAlignment",H="imageRepeat",I="auto",J="favicon",K="sketchFactor",L="0",M="colorStyle",N="appliedColor",O="fontName",P="Applied Font",Q="borderWidth",R="adaptiveStyles",S="interactionMap",T="diagram",U="objects",V="masters",W="objectPaths";
+return _creator();
+})());
